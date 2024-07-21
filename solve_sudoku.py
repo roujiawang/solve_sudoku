1d8ee7d649e422368806b641cf97eb0d0ce08f31
Roujia <rjwang@umich.edu>
2024-07-21 14:10:10 -0400

    enabled two options for sudoku board generation results: either unique solution, or includes <num> initial numbers

--- solve_sudoku.py
+++ solve_sudoku.py
@@ -31,40 +31,121 @@ def solve_sudoku(board, row=0, col=0):
     return False
 
 
-def generate_full_sudoku():
-    board = [[0 for _ in range(9)] for _ in range(9)]
-    solve_sudoku(board)
-    return board
-
-
-def generate_sudoku_with_clues(clues=17, seed=None):
-    # Use seed to avoid deterministic generation
+def generate_sudoku(seed=None, initial_number_count=None):
     if seed is not None:
         random.seed(seed)
     else:
         random.seed(time.time())
 
-    # Generate a full Sudoku board
-    full_board = generate_full_sudoku()
+    board = [
+        [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [4, 5, 6, 7, 8, 9, 1, 2, 3],
+        [7, 8, 9, 1, 2, 3, 4, 5, 6],
+        [2, 3, 1, 5, 6, 4, 8, 9, 7],
+        [5, 6, 4, 8, 9, 7, 2, 3, 1],
+        [8, 9, 7, 2, 3, 1, 5, 6, 4],
+        [3, 1, 2, 6, 4, 5, 9, 7, 8],
+        [6, 4, 5, 9, 7, 8, 3, 1, 2],
+        [9, 7, 8, 3, 1, 2, 6, 4, 5],
+    ]
 
-    # Remove numbers to leave exactly `clues` clues
-    sudoku_with_clues = remove_numbers(full_board, clues=clues)
+    def shuffle_numbers():
+        for i in range(1, 10):
+            n1 = i
+            n2 = random.randint(1, 9)
+            for y in range(9):
+                for x in range(9):
+                    if board[y][x] == n1:
+                        board[y][x] = n2
+                    elif board[y][x] == n2:
+                        board[y][x] = n1
 
-    return sudoku_with_clues
+    def shuffle_rows():
+        for i in range(9):
+            block = i // 3
+            r2 = block * 3 + random.randint(0, 2)
+            board[i], board[r2] = board[r2], board[i]
 
-def remove_numbers(board, clues=17):
-    positions = [(r, c) for r in range(9) for c in range(9)]
-    random.shuffle(positions)
-    removed = 0
+    def shuffle_cols():
+        for i in range(9):
+            block = i // 3
+            c1 = i
+            c2 = block * 3 + random.randint(0, 2)
+            for r in range(9):
+                board[r][c1], board[r][c2] = board[r][c2], board[r][c1]
 
-    while removed < 81 - clues:
-        row, col = positions.pop()
-        if board[row][col] != 0:
+    def shuffle_3x3_rows():
+        for i in range(3):
+            r1 = i * 3
+            r2 = random.randint(0, 2) * 3
+            for j in range(3):
+                board[r1 + j], board[r2 + j] = board[r2 + j], board[r1 + j]
+
+    def shuffle_3x3_cols():
+        for i in range(3):
+            c1 = i * 3
+            c2 = random.randint(0, 2) * 3
+            for j in range(3):
+                for r in range(9):
+                    board[r][c1 + j], board[r][c2 + j] = board[r][c2 + j], board[r][c1 + j]
+
+    def remove_numbers():
+        positions = [(r, c) for r in range(9) for c in range(9)]
+        random.shuffle(positions)
+        while positions:
+            row, col = positions.pop()
+            backup = board[row][col]
             board[row][col] = 0
-            removed += 1
-        
-        # here we don't require the solution 
-        # to the board to be unique
+            if count_solutions(board) != 1:
+                board[row][col] = backup
+    
+    def remove_numbers_until(clues=17):
+        positions = [(r, c) for r in range(9) for c in range(9)]
+        random.shuffle(positions)
+        removed = 0
+
+        while removed < 81 - clues:
+            row, col = positions.pop()
+            if board[row][col] != 0:
+                board[row][col] = 0
+                removed += 1
+
+        return board
+
+    def count_solutions(board):
+        solutions = 0
+
+        def solve_and_count(board, row=0, col=0):
+            nonlocal solutions
+            if row == 9:
+                solutions += 1
+                return True
+            if col == 9:
+                return solve_and_count(board, row + 1, 0)
+            if board[row][col] != 0:
+                return solve_and_count(board, row, col + 1)
+            for num in range(1, 10):
+                if is_safe(board, row, col, num):
+                    board[row][col] = num
+                    if solve_and_count(board, row, col + 1):
+                        if solutions > 1:
+                            return False
+                    board[row][col] = 0
+            return False
+
+        solve_and_count(board)
+        return solutions
+
+    shuffle_numbers()
+    shuffle_rows()
+    shuffle_cols()
+    shuffle_3x3_rows()
+    shuffle_3x3_cols()
+    if initial_number_count:
+        print("here!")
+        remove_numbers_until(clues=initial_number_count)
+    else:
+        remove_numbers()
 
     return board
 
@@ -85,7 +166,7 @@ def print_board(board):
 if __name__ == "__main__":
     # Generate a full Sudoku board
     # Remove numbers to leave exactly 17 clues
-    sudoku_board_with_17_clues = generate_sudoku_with_clues()
+    sudoku_board_with_17_clues = generate_sudoku(initial_number_count=17)
 
     # Print the generated board
     print("Board generated:")
@@ -94,5 +175,19 @@ if __name__ == "__main__":
     if solve_sudoku(sudoku_board_with_17_clues):
         print("One solution for this board:")
         print_board(sudoku_board_with_17_clues)
+    else:
+        print("No solution exists for this board.")
+
+    # Generate a full Sudoku board
+    # Remove numbers until the solution is detected to be no longer unique
+    sudoku_board = generate_sudoku()
+
+    # Print the generated board
+    print("Board generated:")
+    print_board(sudoku_board)
+
+    if solve_sudoku(sudoku_board):
+        print("One solution for this board:")
+        print_board(sudoku_board)
     else:
         print("No solution exists for this board.")
